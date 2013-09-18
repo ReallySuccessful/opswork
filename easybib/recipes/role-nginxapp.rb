6598b81399724da4edf7e1081404addcfa45bce0
David Lemarier <david@reallysuccessful.com>
2013-09-18 11:50:47 -0400

    Update role-nginxapp.rb

--- easybib/recipes/role-nginxapp.rb
+++ easybib/recipes/role-nginxapp.rb
@@ -8,7 +8,7 @@ include_recipe "php-pear"
 include_recipe "php-phar"
 include_recipe "php-suhosin"
 #include_recipe "php-gearman"
-include_recipe "unfuddle-ssl-fix::install"
+#include_recipe "unfuddle-ssl-fix::install"
 #include_recipe "deploy::easybib"
 
 include_recipe "nginx-app::configure"
