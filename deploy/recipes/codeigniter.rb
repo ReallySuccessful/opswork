76fc7e8bdf6987d4c145f3e7ad15f218ef752866
David Lemarier <david@reallysuccessful.com>
2013-09-18 14:47:34 -0400

    Update codeigniter.rb

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -1,21 +1,36 @@
-include_recipe 'deploy'
-include_recipe "nginx::service"
+include_recipe "php-fpm::service"
 
-node[:deploy].each do |application, deploy|
+instance_roles = get_instance_roles()
+cluster_name   = get_cluster_name()
+
+node["deploy"].each do |application, deploy|
+
+  Chef::Log.info("deploy::easybib - app: #{application}, role: #{instance_roles}")
+
+  next unless deploy["deploying_user"]
+  next unless instance_roles.include?('opswork')
+
+  Chef::Log.info("deploy::easybib - Deployment started.")
+  Chef::Log.info("deploy::easybib - Deploying as user: #{deploy["user"]} and #{deploy["group"]}")
+
+  opsworks_deploy_user do
+    deploy_data deploy
+    app application
+  end
+
   opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
+    user  deploy["user"]
+    group deploy["group"]
+    path  deploy["deploy_to"]
   end
 
   opsworks_deploy do
-    app application
     deploy_data deploy
+    app application
   end
 
-#  ci_web_app application do
-#    application deploy
-#    cookbook "nginx-app"
-#  end
-  
+  service "php-fpm" do
+    action :reload
+  end
+
 end
