e43a0dcb581a89ca7b84d3e07f43d18e83f75eeb
David Lemarier <david@reallysuccessful.com>
2013-09-18 15:05:02 -0400

    Update codeigniter.rb

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -3,10 +3,21 @@ include_recipe "php-fpm::service"
 instance_roles = get_instance_roles()
 cluster_name   = get_cluster_name()
 
-node["deploy"].each do |application, deploy|
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
 
-  Chef::Log.info("deploy::leadrush - app: #{application}, role: #{instance_roles}")
-  Chef::Log.info(deploy)
+#  opsworks_deploy do
+#    app application
+#    deploy_data deploy
+#  end
 
-
+#  ci_web_app application do
+#    application deploy
+#    cookbook "nginx-app"
+#  end
+  
 end
