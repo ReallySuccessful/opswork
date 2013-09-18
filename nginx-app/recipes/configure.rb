d5cfe220c51385c9c74d37b27209106f16a948f7
David Lemarier <david@reallysuccessful.com>
2013-09-18 11:25:28 -0400

    Update configure.rb

--- nginx-app/recipes/configure.rb
+++ nginx-app/recipes/configure.rb
@@ -19,46 +19,14 @@ nginx_config = "easybib.com.conf.erb"
 
 node["deploy"].each do |application, deploy|
 
-  case application
-  when 'easybib'
-    if !node["nginx-lb"]["cluster"].include?(cluster_name)
-      next
-    end
-    if !instance_roles.include?('nginxphpapp') && !instance_roles.include?('testapp')
-      next
-    end
-
-  when 'easybib_api'
-    next unless instance_roles.include?('bibapi')
-
-  when 'infolit'
-    next unless cluster_name == 'InfoLit'
-    next unless instance_roles.include?('nginxphpapp')
-    nginx_config = "infolit.conf.erb"
-
-  when 'sitescraper'
-    next unless instance_roles.include?('sitescraper')
-
-  when 'research_app'
-    next unless cluster_name == 'Research Cloud'
-    next unless instance_roles.include?('nginxphpapp')
-
-  else
-    Chef::Log.debug("Skipping nginx-app::configure for app #{application}")
-    next
-  end
-
   php_upstream = "unix:/var/run/php-fpm/#{node["php-fpm"]["user"]}"
 
-  template "#{nginx_config_dir}/sites-enabled/easybib.com.conf" do
+  template "#{nginx_config_dir}/sites-enabled/#{application}.conf" do
     source nginx_config
     mode   "0755"
     owner  node["nginx-app"]["user"]
     group  node["nginx-app"]["group"]
     variables(
-      :js_alias           => node["nginx-app"]["js_modules"],
-      :img_alias          => node["nginx-app"]["img_modules"],
-      :css_alias          => node["nginx-app"]["css_modules"],
       :access_log         => app_access_log,
       :deploy             => deploy,
       :application        => application,
