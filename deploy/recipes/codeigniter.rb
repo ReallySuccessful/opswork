85b50c18a7b05926b2dc9aa8a77c30a72b4c9cd0
david@reallysuccessful.com <david@wasabi20.com>
2013-09-23 13:17:30 -0400

    Added development layer // develop branch with automatic setup of domains

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -6,12 +6,23 @@ node[:deploy].each do |application, deploy|
 
   app_role = deploy[:deploy_to]
 
-  Chef::Log.debug("[LEADRUSH] APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
+if !instance_layer.include?("develop")
+
+  Chef::Log.debug("[LEADRUSH] PRODUCTION MODE //  APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
 
   if !instance_layer.include?(app_role)
       Chef::Log.debug("[LEADRUSH] SKIPPING APP #{application} DEPLOYMENT - INVALID ROLE FOR THIS INSTANCE")
       next
   end
+
+else
+
+  Chef::Log.debug("[LEADRUSH] DEVELOPMENT MODE // APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
+
+  deploy[:revision] = "develop"
+  deploy[:domains] = deploy[:beta_domains]
+
+end  
 
   ci_deploy_dir do
     user deploy[:user]
