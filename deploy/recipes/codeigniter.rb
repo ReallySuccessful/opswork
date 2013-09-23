f1a5bc90c0a9e57c23a600b97431d7dbd5628fcc
david@reallysuccessful.com <david@wasabi20.com>
2013-09-23 12:06:53 -0400

    Added role in each app / layer for deployment

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -1,6 +1,18 @@
 include_recipe "php-fpm::service"
 
+instance_layer = node["opsworks"]["instance"]["layers"]
+
 node[:deploy].each do |application, deploy|
+
+  app_role = deploy[:deploy_to]
+
+  Chef::Log.debug("[LEADRUSH] APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
+
+  if !instance_layer.include?(app_role)
+      Chef::Log.debug("[LEADRUSH] SKIPPING APP #{application} DEPLOYMENT - INVALID ROLE FOR THIS INSTANCE")
+      next
+  end
+
   ci_deploy_dir do
     user deploy[:user]
     group deploy[:group]
