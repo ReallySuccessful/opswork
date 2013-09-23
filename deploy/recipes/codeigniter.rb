c98a4d9494842614b6a3a79d838357bcfc4c9e6f
david@reallysuccessful.com <david@wasabi20.com>
2013-09-23 15:23:21 -0400

    Fix deploy_to

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -20,8 +20,10 @@ node[:deploy].each do |application, deploy|
 
     Chef::Log.debug(deploy)
     Chef::Log.debug("[LEADRUSH] DEVELOPMENT MODE // APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
-    node.deploy["revision"] = "develop"
-    node.deploy["domains"] = deploy[:beta_domains]
+    
+    node.default.deploy["revision"] = "develop"
+    node.default.deploy["domains"] = deploy[:beta_domains]
+
     Chef::Log.debug("[LEADRUSH] BETA DOMAINS:")
     Chef::Log.debug(deploy[:domains])
 
