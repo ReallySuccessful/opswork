81a39497fc343b7ef7310dc41a92ef7386b65bf3
david@reallysuccessful.com <david@wasabi20.com>
2013-09-23 13:31:48 -0400

    Fix: Chef::Exceptions::ImmutableAttributeModification: Node attributes are read-only

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -6,23 +6,23 @@ node[:deploy].each do |application, deploy|
 
   app_role = deploy[:deploy_to]
 
-if !instance_layer.include?("develop")
+  if !instance_layer.include?("develop")
 
-  Chef::Log.debug("[LEADRUSH] PRODUCTION MODE //  APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
+    Chef::Log.debug("[LEADRUSH] PRODUCTION MODE //  APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
 
-  if !instance_layer.include?(app_role)
-      Chef::Log.debug("[LEADRUSH] SKIPPING APP #{application} DEPLOYMENT - INVALID ROLE FOR THIS INSTANCE")
-      next
-  end
+    if !instance_layer.include?(app_role)
+        Chef::Log.debug("[LEADRUSH] SKIPPING APP #{application} DEPLOYMENT - INVALID ROLE FOR THIS INSTANCE")
+        next
+    end
 
-else
+  else
 
-  Chef::Log.debug("[LEADRUSH] DEVELOPMENT MODE // APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
+    Chef::Log.debug("[LEADRUSH] DEVELOPMENT MODE // APP ROLE: #{app_role} ON LAYER: #{instance_layer}")
 
-  deploy[:revision] = "develop"
-  deploy[:domains] = deploy[:beta_domains]
+    node.deploy[:revision] = "develop"
+    node.deploy[:domains] = deploy[:beta_domains]
 
-end  
+  end  
 
   ci_deploy_dir do
     user deploy[:user]
