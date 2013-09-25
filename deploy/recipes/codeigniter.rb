8f02be43a448ca45bba44e5c14d4e75d6134ac2b
david@reallysuccessful.com <david@wasabi20.com>
2013-09-25 06:43:08 -0400

    Fix beta server generation

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -24,9 +24,12 @@ node[:deploy].each do |application, deploy_data|
 
     node.override['deploy_data']['revision'] = "develop"
     node.override['deploy_data']['domains'] = deploy_data[:domains]
+    node.override['deploy_data'][:domains] = deploy_data[:domains]
 
     Chef::Log.debug("[LEADRUSH] DEPLOY #{deploy_data[:revision]} BETA DOMAINS:")
     Chef::Log.debug(deploy_data[:domains])
+    Chef::Log.debug(deploy_data)
+    Chef::Log.debug(deploy_data['domains'])
 
   end  
 
