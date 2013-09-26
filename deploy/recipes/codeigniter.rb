2f5956004fd83e5151f51fda7f3249e69933bf0d
david@reallysuccessful.com <david@wasabi20.com>
2013-09-26 12:30:59 -0400

    Update cookbooks for deployment hook

--- deploy/recipes/codeigniter.rb
+++ deploy/recipes/codeigniter.rb
@@ -47,41 +47,66 @@ node[:deploy].each do |application, deploy_data|
     cookbook "nginx-app"
   end
 
-  find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git describe --tags")
-  find.run_command    
-  tag = find.stdout
-  find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git log --oneline -1")
-  find.run_command    
-  last_commit = find.stdout
-
-  all_results[application] = {
-    "branch" => deploy_branch,
-    "domains" => deploy_domains,
-    "running_version" => tag,
-    "last_commit" => last_commit  
-  }
-
 end
 
-message_to_send = { 
-  :message => "Deployment complete,",
-  :applications => all_results, 
-  :server_details => {
-      :hostname => node[:opsworks][:instance][:hostname],
-      :instance_id => node[:opsworks][:instance][:id],
-      :instance_type => node[:opsworks][:instance][:instance_type],
-      :public_ip => node[:opsworks][:instance][:ip],
-      :layer => instance_layer
-  } 
-}
 
-# send post to MAMA
-http_request "Alerting mama !" do
-  action :post
-  url "http://mamabot.herokuapp.com/webhook/dev"
-  message message_to_send
+ruby_block "Send message to MAMA once everything is done and restart NGINX :)" do
+  block do
+
+    node[:deploy].each do |application, deploy_data|
+      if !instance_layer.include?("develop")
+        if !instance_layer.include?(app_role)
+            next
+        end
+
+        deploy_domains = deploy_data[:domains]
+        deploy_branch = "master"
+
+      else
+
+        deploy_domains= deploy_data[:beta_domains]
+        deploy_branch = "develop"
+
+      end  
+      find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git describe --tags")
+      find.run_command    
+      tag = find.stdout
+      find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git log --oneline -1")
+      find.run_command    
+      last_commit = find.stdout
+
+      all_results[application] = {
+        "branch" => deploy_branch,
+        "domains" => deploy_domains,
+        "running_version" => tag,
+        "last_commit" => last_commit  
+      }     
+       
+    end  
+
+    message_to_send = { 
+      :message => "Deployment complete,",
+      :applications => all_results, 
+      :server_details => {
+          :hostname => node[:opsworks][:instance][:hostname],
+          :instance_id => node[:opsworks][:instance][:id],
+          :instance_type => node[:opsworks][:instance][:instance_type],
+          :public_ip => node[:opsworks][:instance][:ip],
+          :layer => instance_layer
+      } 
+    }
+
+    # send post to MAMA
+    http_request "Alerting mama !" do
+      action :post
+      url "http://mamabot.herokuapp.com/webhook/dev"
+      message message_to_send
+    end
+
+    execute "nginx restart" do
+      command "/etc/init.d/nginx restart"
+    end
+
+  end
 end
 
-execute "nginx restart" do
-  command "/etc/init.d/nginx restart"
-end
