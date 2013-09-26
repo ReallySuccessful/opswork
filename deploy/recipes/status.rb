26d628fcbaf67d280dd052736b27f32d913a5113
david@reallysuccessful.com <david@wasabi20.com>
2013-09-26 08:29:37 -0400

    Final changes for getting status of OpsWorks app deployed

--- deploy/recipes/status.rb
+++ deploy/recipes/status.rb
@@ -6,8 +6,6 @@ all_results = {}
 
 node[:deploy].each do |application, deploy_data|
 
-	all_results[application] = {}
-
 	app_role = deploy_data[:deploy_layer]
 
 	if !instance_layer.include?("develop")
@@ -28,13 +26,13 @@ node[:deploy].each do |application, deploy_data|
 	find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git describe --tags")
     find.run_command    
     tag = find.stdout
-    find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git show --summary")
+    find = Mixlib::ShellOut.new("cd /srv/www/#{application}/current && git log --oneline -1")
     find.run_command    
     last_commit = find.stdout
-    hostname = node['hostname']
 
-	all_results[application][hostname] = {
-	    "version" => tag,
+	all_results[application] = {
+	    "server" => node['hostname'],
+	    "running_version" => tag,
 		"last_commit" => last_commit,
 		"branch" => deploy_branch,
 		"domains" => deploy_domains
