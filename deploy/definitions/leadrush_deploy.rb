b65033046de74efcac1c6731832559e2410b72b0
david@reallysuccessful.com <david@wasabi20.com>
2013-10-03 12:56:45 -0400

    Added layer in config file

--- deploy/definitions/leadrush_deploy.rb
+++ deploy/definitions/leadrush_deploy.rb
@@ -135,6 +135,7 @@ define :leadrush_deploy do
               :memcached => node[:deploy][application][:memcached],
               :layers => node[:opsworks][:layers],
               :stack_name => node[:opsworks][:stack][:name],
+              :layer => node[:opsworks][:instance][:layers]
               # TODO: we need to found a way to extract the current git tag
               :revision => find.stdout
             )
