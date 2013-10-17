e02def54343ba4c9dfdbc88874f8259314e2c000
david@reallysuccessful.com <david@wasabi20.com>
2013-10-17 09:56:51 -0400

    Added php gearman cookbook

--- php-gearman/recipes/pecl.rb
+++ php-gearman/recipes/pecl.rb
@@ -1,7 +1,6 @@
 # This recipe is necessary to do a simple `pecl install gearman` on Ubuntu Lucid
 include_recipe "php-fpm::source"
-
-php_pecl "gearman" do
+php_pear "gearman" do
   version node["gearman"]["pecl"]
   action [ :install, :setup ]
 end
